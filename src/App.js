4318e374b8cc0e8a6ad6245916cc54e6a05642db
Mustafa Adam Alshammaa <adam.benals@gmail.com>
2021-12-11 00:26:18 +0100

    Sidebar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <h2>Let's build a Gmail clone</h2>
+      <Sidebar />
     </div>
   );
 }
